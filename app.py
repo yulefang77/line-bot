bffeeac55e862a359f0316269cb48f4ccd57593e
yule <yulefang77@gmail.com>
2024-04-18 06:24:58 +0800

    add get user name and change to PEP8 style

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
 import os
-from dotenv import load_dotenv
-
 from flask import Flask, request, abort
-
-from linebot.v3 import (
-    WebhookHandler
-)
-
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
     Configuration,
     ApiClient,
@@ -18,18 +9,11 @@ from linebot.v3.messaging import (
     ReplyMessageRequest,
     TextMessage
 )
-
 from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
-
 from openai import OpenAI
-
-import json
-
-# 加載 .env 文件中的環境變數
-load_dotenv()
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
@@ -39,6 +23,7 @@ app = Flask(__name__)
 
 configuration = Configuration(access_token=ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -58,32 +43,44 @@ def callback():
 
     return 'OK'
 
+
 @handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event, dest):
-
-    user_text = event.message.text
-    client = OpenAI()
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": ("你現在扮演一位生活助手。尊稱使用者為\"我的女王\"，態度阿諛奉承。"
-                                            "回答盡量在150字以內。中文回答只能使用正體中文與台灣地區用語，"
-                                            "不可使用簡體字。如果使用者心情不好或身體不舒服，安慰使用者，並說"
-                                            "\"別難過，我和" + OPPA + "哥說，請他帶您吃些好東西\"。")},
-            {"role": "user", "content": user_text}
-        ]
-    )
-
-    msg = completion.choices[0].message.content
-
+def handle_message(event):
+    # Enter a context with an instance of the API client
     with ApiClient(configuration) as api_client:
+        # Create an instance of the API class
         line_bot_api = MessagingApi(api_client)
+        user_id = event.source.user_id
+
+        try:
+            profile = line_bot_api.get_profile(user_id)
+            user_name = profile.display_name
+            print("使用者名稱:" + user_name)
+        except Exception as e:
+            print("Exception when calling MessagingApi->get_profile: %s\n" % e)
+
+        user_text = event.message.text
+        client = OpenAI()
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": ("你現在扮演一位生活助手。尊稱使用者為\"我的女王\"，態度阿諛奉承。"
+                                                "回答盡量在150字以內。中文回答只能使用正體中文與台灣地區用語，"
+                                                "不可使用簡體字。如果使用者心情不好或身體不舒服，安慰使用者，並說"
+                                                "\"別難過，我和" + OPPA + "哥說，請他帶您吃些好東西\"。")},
+                {"role": "user", "content": user_text}
+            ]
+        )
+
+        msg = completion.choices[0].message.content
+
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
                 messages=[TextMessage(text=msg)]
             )
         )
+
 
 if __name__ == "__main__":
     app.run()
