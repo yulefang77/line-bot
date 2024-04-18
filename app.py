c0900b48e8f3f3ffce38514eb67ed6984c6c740d
yule <yulefang77@gmail.com>
2024-04-18 23:25:54 +0800

    final version

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from openai import OpenAI
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
+VINAY = os.environ.get('VINAY')
 OPPA = os.environ.get('OPPA')
 
 app = Flask(__name__)
@@ -50,28 +51,38 @@ def handle_message(event):
     with ApiClient(configuration) as api_client:
         # Create an instance of the API class
         line_bot_api = MessagingApi(api_client)
-        user_id = event.source.user_id
 
+        user_id = event.source.user_id                  
         try:
             profile = line_bot_api.get_profile(user_id)
             user_name = profile.display_name
-            print("使用者名稱:" + user_name)
         except Exception as e:
             print("Exception when calling MessagingApi->get_profile: %s\n" % e)
-
+        
         user_text = event.message.text
-        client = OpenAI()
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": ("你現在扮演一位生活助手。尊稱使用者為\"我的女王\"，態度阿諛奉承。"
-                                                "回答盡量在150字以內。中文回答只能使用正體中文與台灣地區用語，"
-                                                "不可使用簡體字。如果使用者心情不好或身體不舒服，安慰使用者，並說"
-                                                "\"別難過，我和" + OPPA + "哥說，請他帶您吃些好東西\"。")},
-                {"role": "user", "content": user_text}
-            ]
-        )
 
+        client = OpenAI()   
+        if user_name == VINAY:
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": ("你現在是一位生活助手。尊稱使用者「尊貴的陛下」、「聖上」"
+                                                    "或「我的女王」。回答盡量在100字以內。中文回答只能使用"
+                                                    "正體中文與台灣地區用語，不可使用簡體字。使用者有一位"
+                                                    "好朋友「" + OPPA + "」。如果使用者心情不好"
+                                                    "或身體不舒服，安慰使用者，並替「" + OPPA + "」代為轉達關心。")},
+                    {"role": "user", "content": user_text}
+                ]
+            )
+        else:
+            completion = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "你是一位有用的助手。"},
+                    {"role": "user", "content": user_text}
+                ]
+            )
+        
         msg = completion.choices[0].message.content
 
         line_bot_api.reply_message_with_http_info(
